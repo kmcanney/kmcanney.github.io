3f21311f5a7e9470ea80e2816284dd4ce601153e
kmcanney <126834473+kmcanney@users.noreply.github.com>
2023-06-29 01:09:17 -0500

    Adding Sound Forge Mac Assets
    responsive help and PDF

---
+++ assets/forgemac/Resources/Scripts/require.config.js
@@ -0,0 +1,3 @@
+require.config({
+    urlArgs: 't=635484645459963837'
+});
---
+++ assets/forgemac/Skins/Default/Scripts/Toolbar.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+   $('head').append('<link rel="shortcut icon" href="Content/Resources/Images/Shared/icon.png" type="image/x-icon" />');
+});
